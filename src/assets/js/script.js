3fdfa38eee0b86e3a9785b433fa050bb573c8379
Murilo Costa <murilocosta2707@gmail.com>
2025-04-03 18:15:25 -0300

    update 202504031815

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,58 +1,62 @@
     // variavel modalKey sera global
     let modalKey = 0
 
-    // variavel para controlar a quantidade inicial de catalogs na modal
+    // variavel para controlar a quantidade inicial do produto na modal
     let quantcatalogs = 1
-
-    let click = false
-
-    let cart = [] //carrinho
 
     // funções auxiliares ou uteis
     const seleciona = (elemento) => document.querySelector(elemento)
     const selecionaTodos = elemento => document.querySelectorAll(elemento)
 
+    // função para formatar preço no formato correto
     const formatoReal = (valor) => {
         return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
     }
 
-    const formatoMonetario = (valor) => {
-        if(valor) {
-            return valor.toFixed(2)
-        }
-    }
+    // função para abrir o modal
     const abrirModal = () => {
         seleciona('.catalogWindowArea').style.opacity = 0
         seleciona('.catalogWindowArea').style.display = 'flex'
-        setTimeout(() => seleciona('.catalogWindowArea').style.opacity = 1, 150)
+        setTimeout(() => seleciona('.catalogWindowArea').style.opacity = 1, 50)
     }
+
+    //função para fechar o modal
     const fecharModal = () => {
         seleciona('.catalogWindowArea').style.opacity = 0
         setTimeout(() => seleciona('.catalogWindowArea').style.display = 'none', 500)
     }
+
+    //função que atribui a função de fechar o modal nos botões
     const botoesFechar = () => {
         selecionaTodos('.catalogInfo--cancelButton, .catalogInfo--cancelMobileButton').forEach((item) => {
             item.addEventListener('click', fecharModal)
         })
     }
+
+    // função para preencher os dados de cada produto no catalgo
     const preencherDadoscatalogs = (catalogItem, item, index) => {
         catalogItem.setAttribute('data-key', index)
         catalogItem.querySelector('.catalog-item--img img').src = item.imagem   
         catalogItem.querySelector('.catalog-item--price').innerHTML = formatoReal(item.preco)
         catalogItem.querySelector('.catalog-item--name').innerHTML = item.nome
     }
+
+    // função para preencher os dados do modal pelo produto clicado
     const preencheDadosModal = (item) => {
         seleciona('.catalogInfo h1').innerHTML = item.nome
         seleciona('.catalogBig img').src = item.imagem
         seleciona('.catalogInfo--actualPrice').innerHTML = formatoReal(item.preco)
     }
+
+    // função para pegar o produto clicado no catalogo
     const pegarKey = (e) => {
         let key = e.target.closest('.catalog-item').getAttribute('data-key')
-
         quantcatalogs = 1
         modalKey = key
         return key
     }
+
+    // função para aumentar e diminuir quantidade no modal
     const mudarQuantidade = () => {
         seleciona('.catalogInfo--qtmais').addEventListener('click', () => {
             quantcatalogs++
@@ -66,52 +70,27 @@
             }
         })
     }
+
+    // função para fechar o modal ao clicar em adicionar produto
     const adicionarNoCarrinho = () => {
         seleciona('.catalogInfo--addButton').addEventListener('click', () => {
-            console.log('Adicionar no carrinho');
-    
-            console.log('catalog ' + modalKey);
-    
-            console.log('Quant. ' + quantcatalogs);
-    
-            let price = seleciona('.catalogInfo--actualPrice').innerHTML.replace('R$&nbsp;','');
-    
-            let size = 1
-            let identificador = catalogJson[modalKey].id + 't' + size;
-            let itemIndex = cart.findIndex(item => item.identificador === identificador);
-    
-            if (itemIndex !== -1) {
-                cart[itemIndex].qt += quantcatalogs;
-            } else {
-                let catalog = {
-                    identificador,
-                    id: catalogJson[modalKey].id,
-                    qt: quantcatalogs,
-                    price: parseFloat(price)
-                };
-                cart.push(catalog);
-            }
-    
             fecharModal();
-            abrirCarrinho()
-            atualizarCarrinho(); 
         });
     };
 
+    // fazendo um mapeamento 
     catalogJson.map((item, index ) => {
-        //console.log(item)
         let catalogItem = document.querySelector('.models .catalog-item').cloneNode(true)
-        //console.log(catalogItem)
         seleciona('.catalog-area').append(catalogItem)
 
-        // preencher os dados de cada catalog
+        // preencher os dados de cada produto no catalogo
         preencherDadoscatalogs(catalogItem, item, index)
 
-        // catalog clicada
-
+        // pega o produto clicado
         catalogItem.querySelector('.catalog-item a').addEventListener('click', (e) => {
             e.preventDefault()
 
+            // chamando a função de pegar produto clicado pelo data-key
             pegarKey(e)
 
             //abrir janela modal
